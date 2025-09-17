6f4418cbae9e4fca858086580c4a0d02ddd913cd
Pujan Mendhule <mendhulepujan@gmail.com>
2025-09-17 09:57:34 +0530

    Fix import path case for Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NOpage from "./pages/NOpage";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
